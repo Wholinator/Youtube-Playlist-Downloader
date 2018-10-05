689de7f3d9056d7a56e11ade7899f182076d7bda
= <=>
2018-10-05 10:04:10 -0500

    Initial powershell web command

---
+++ Dl.py
@@ -0,0 +1,26 @@
+import subprocess
+
+# url of playlist to download
+url = "https://www.youtube.com/watch?v=tRhTe8rZHQc&list=PLQiJ2_8qupzdyUj7ZX7ujDNtFrbfwXKLR"
+
+# powershell command to be later executed
+power = "$Playlist = ((Invoke-WebRequest \"" + url + """\").Links | Where {$_.class -match "playlist-video"}).href
+
+ForEach ($Video in $Playlist) {
+$s ="https://www.youtube.com" + $Video
+$s =$s.Substring(0, $s.IndexOf('&'))
+    Write-Output ($s)
+}"""
+
+# deletes file contents
+s = open("out.txt", 'a')
+s.seek(0)
+s.truncate()
+s.close()
+
+# execute the powershell script to grab the video urls
+t = subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe", power], stdout=open("out.txt", "w+"))
+
+# reopen our file to read from
+s = open("out.txt", "r")
+
